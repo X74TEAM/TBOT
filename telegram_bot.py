4f5abd39eb2581a5826d47f876b8127ff5b6bfd6
X74 <136961771+X74TEAM@users.noreply.github.com>
2024-06-17 14:20:48 +0600

    Add files via upload

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,28 +1,71 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# pylint: disable=unused-argument
+# This program is dedicated to the public domain under the CC0 license.
 
-# This is a set_my_commands example.
-# Press on [/] button in telegram client.
-# Important, to update the command menu, be sure to exit the chat with the bot and enter to chat again
-# Important, command for chat_id and for group have a higher priority than for all
+"""
+Simple Bot to reply to Telegram messages.
 
-import telebot
+First, a few handler functions are defined. Then, those functions are passed to
+the Application and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
 
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
 
-API_TOKEN = '7201274655:AAGtMzZy40Lu4A1T3v_42hW1GrOizHQMZu8'
-bot = telebot.TeleBot(API_TOKEN)
+import logging
 
-# use in for delete with the necessary scope and language_code if necessary
-bot.delete_my_commands(scope=None, language_code=None)
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
-bot.set_my_commands(
-    commands=[
-        telebot.types.BotCommand("command1", "command1 description"),
-        telebot.types.BotCommand("command2", "command2 description")
-    ],
-    # scope=telebot.types.BotCommandScopeChat(12345678)  # use for personal command for users
-    # scope=telebot.types.BotCommandScopeAllPrivateChats()  # use for all private chats
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
-# check command
-cmd = bot.get_my_commands(scope=None, language_code=None)
-print([c.to_json() for c in cmd])
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("Help!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    await update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token("TOKEN").build()
+
+    # on different commands - answer in Telegram
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+
+    # on non command i.e message - echo the message on Telegram
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
