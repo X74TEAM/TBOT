ac2ccb82650303993f0803d885861e9a125204d4
X74 <136961771+X74TEAM@users.noreply.github.com>
2024-06-19 12:51:15 +0600

    Add files via upload

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,12 +1,75 @@
+#!/usr/bin/env python
+# pylint: disable=unused-argument
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Basic example for a bot that uses inline keyboards. For an in-depth explanation, check out
+ https://github.com/python-telegram-bot/python-telegram-bot/wiki/InlineKeyboard-Example.
+"""
+import logging
+
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes, MessageHandler, ConversationHandler, CallbackContext, filters
+
+
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_html(f'<b>Hey, @{update.effective_user.username}! Welcome to xEarningBot!</b>\nAre you ready to Earn using our bot.?')
+    """Sends a message with three inline buttons attached."""
+    keyboard = [
+    [InlineKeyboardButton(text='Start Now! 🎲', url='http://t.me/xEarningBot/run')],
+    [InlineKeyboardButton(text='Join Community 🫂', url='https://t.me/gotaptap')],
+    [InlineKeyboardButton("Help 😣", callback_data="/help")],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    user = update.effective_user
+    await update.message.reply_html(f'<b>Hey, {user.mention_html()}! Welcome to xEarningBot!</b> \nAre you ready to Earn using our bot.?', reply_markup=reply_markup)
 
 
-app = ApplicationBuilder().token("7201274655:AAGtMzZy40Lu4A1T3v_42hW1GrOizHQMZu8").build()
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
 
-app.add_handler(CommandHandler("start", start))
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    await query.answer()
 
-app.run_polling()
+    await query.edit_message_text(text=f"{query.data}")
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Displays info on how to use the bot."""
+    await update.message.reply_text("Use /start to run this bot.")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    await update.message.reply_text(update.message.text)
+    
+def main() -> None:
+    application = Application.builder().token("7201274655:AAGtMzZy40Lu4A1T3v_42hW1GrOizHQMZu8").build()
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CallbackQueryHandler(button))
+    application.add_handler(CommandHandler("help", help_command))
+
+
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
